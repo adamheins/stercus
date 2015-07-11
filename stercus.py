15ca1c4f2beb5ba5a34b5a9780008d4fe829205b
Adam Heins <mail@adamheins.com>
2015-07-11 03:09:02 -0400

    Update compiler with custom application support.

--- stercus.py
+++ stercus.py
@@ -3,6 +3,7 @@
 import argparse
 import re
 import sys
+from subprocess import call
 
 # Size of data available to the program (in bytes).
 DATA_SIZE = 10000
@@ -33,8 +34,12 @@ class UnbalancedBracketsError(Exception):
     """ Raised when the program has an unbalanced number of brackets. """
     pass
 
+class FunctionNameError(Exception):
+    """ Raised when a disallowed function name is encountered. """
+    pass
 
-def preprocess(src_file):
+
+def preprocess(src):
     """ Preprocessing step """
 
     def remove_comments(src):
@@ -89,98 +94,61 @@ def preprocess(src_file):
             raise UnbalancedBracketsError('Conditional brackets are not '
                     'balanced.')
 
+    def tokenize(src):
+        """ Convert the source string to a list of tokens. """
+        processed_src = ''
+        for char in src:
+            if char in '[](){}':
+                processed_src += ' ' + char + ' '
+            elif char == '$':
+                processed_src += '_s'
+            else:
+                processed_src += char
+        return processed_src.split()
 
-    # Preprocessing rules.
-    PREPROCESS_RULES = {
-            ' [ ': re.compile('(\s*)\[(\s*)'),
-            ' ] ': re.compile('(\s*)\](\s*)'),
-            ' ( ': re.compile('(\s*)\((\s*)'),
-            ' ) ': re.compile('(\s*)\)(\s*)'),
-    }
+    def check_func_name(name):
+        """ Check if the function name is valid. """
+        if not name.isalpha():
+            raise FunctionNameError('Unacceptable function name: ' + name
+                    + '. Function names may only contain letters.')
+        return name
 
-    with open(src_file, 'r') as f:
-        src = f.read()
+    def parse_functions(tokens):
+        """ Parse the functions in the program. """
+        functions = {}
+        open_index = 0
+        index = 0
+        while index < len(tokens):
+            if tokens[index]== '{':
+                open_index = index
+            elif tokens[index] == '}':
+                # Check the function name.
+                func_name = tokens[open_index + 1]
+                check_func_name(func_name)
+
+                # Add the function to the function table.
+                functions[func_name] = tokens[open_index + 2 : index]
+
+                # Remove the function body from the tokens.
+                tokens = tokens[:open_index] + tokens[index + 1:]
+                index = open_index
+                continue
+            index += 1
+        return tokens, functions
 
     src = remove_comments(src)
-
     check_bracket_balance(src)
+    tokens = tokenize(src)
+    tokens, functions = parse_functions(tokens)
 
-    # Add space between all tokens.
-    for key, val in PREPROCESS_RULES.iteritems():
-        src = re.sub(val, key, src)
+    return tokens, functions
 
-    # Substitute all whitespace sequences for a single space, then tokenize
-    # the source.
-    return re.sub(r'\s+', ' ', src).strip().split(' ')
+def compile_function(name, args, tokens, func_list):
+    """ Compile a function from stercus to C. """
 
-def run(tokens):
     def apply(application, accessor):
-        if application == NOP:
-            pass
-        elif application == INCREMENT:
-            data[accessor] += 1
-        elif application == DECREMENT:
-            data[accessor] -= 1
-        elif application == OUTPUT:
-            print data[accessor]
-        elif application == INPUT:
-            data[accessor] = ord(sys.stdin.read(1))
-        else:
-            data[accessor] = int(application)
-
-    def push(token, index):
-        try:
-            value = int(token)
-            if len(stack) > 0 and data[value] == 0 and stack[-1] == CONDITIONAL_START:
-                stack.pop()
-                while tokens[index] != CONDITIONAL_END:
-                    index += 1
-            else:
-                stack.append(token)
-        except:
-            stack.append(token)
-        return index
-
-    # The memory we are working with.
-    data = [0] * (DATA_SIZE + 1)
-
-    stack = []
-    loop_index_stack = []
-
-    index = 0
-    while (index < len(tokens)):
-        token = tokens[index]
-        if token == APPLICATOR_END:
-            li = []
-            val = stack.pop()
-            while val != APPLICATOR_START:
-                li.append(val)
-                val = stack.pop()
-            li.reverse()
-            accessor = int(li[0])
-            for application in li[1:]:
-                apply(application, accessor)
-            index = push(accessor, index)
-        elif token == CONDITIONAL_END:
-            li = []
-            val = stack.pop()
-            while (val != CONDITIONAL_START):
-                li.append(val)
-                val = stack.pop()
-            accessor = int(li[-1])
-            if data[accessor] != 0:
-                index = loop_index_stack.pop()
-                continue
-        elif token == CONDITIONAL_START:
-            loop_index_stack.append(index)
-            stack.append(token)
-        else:
-            index = push(token, index)
-        index += 1
-
-def compile(tokens, out_file):
-    def apply(application, accessor):
-        ele = 'd[' + accessor + ']';
+        """ Apply an application to the accessed location. """
+        ele = '_d[' + accessor + ']';
         if application == NOP:
             pass
         elif application == INCREMENT:
@@ -191,14 +159,14 @@ def compile(tokens, out_file):
             output.append('putchar(' + ele + ');')
         elif application == INPUT:
             output.append(ele + ' = getchar();')
+        elif application in func_list:
+            output.append(application + '(' + accessor + ');')
         else:
             output.append(ele + ' = ' + application + ';')
 
-    output = ['#include <stdio.h>\n#include <stdlib.h>\n']
-    output.append('int main() {')
-    output.append('unsigned char *d = (unsigned char *)calloc('
-            + str(DATA_SIZE) + ', 1);')
-
+    output = ['void ' + name + '(' + args + ') {']
+    if name == 'main':
+        output.append('_d=(unsigned char *)calloc(' + str(DATA_SIZE) + ', 1);')
     stack = []
 
     for token in tokens:
@@ -213,8 +181,8 @@ def compile(tokens, out_file):
             for application in li[1:]:
                 apply(application, accessor)
             if len(stack) > 0 and stack[-1] == CONDITIONAL_START:
-                output.append('if(!d[d[' + accessor + ']]){break;}')
-            stack.append('d[' + accessor + ']')
+                output.append('if(!_d[_d[' + accessor + ']]){break;}')
+            stack.append('_d[' + accessor + ']')
         elif token == CONDITIONAL_END:
             val = stack.pop()
             while val != CONDITIONAL_START:
@@ -228,28 +196,56 @@ def compile(tokens, out_file):
         else:
             # Check if the token is the accessor for a conditional.
             if len(stack) > 0 and stack[-1] == CONDITIONAL_START:
-                output.append('if(!d[' + token + ']){break;}')
+                output.append('if(!_d[' + token + ']){break;}')
             stack.append(token)
 
-    output.append('return 0;\n}')
-    with open(out_file, 'w') as out:
-        out.write('\n'.join(output))
+    output.append('}')
+    return '\n'.join(output)
 
 def main():
+    """ Main entry point for stercus compilation. """
 
+    # Parse command line arguments.
     parser = argparse.ArgumentParser()
     parser.add_argument('src', help='Stercus source file.')
     parser.add_argument('-c', '--compile', help='Compile the stercus file to C,'
                         ' rather than interpreting it.', dest='compile',
                         action='store_true')
+    parser.add_argument('--gcc', help='Compile directly to native code using'
+                        ' GCC.', dest='gcc', action='store_true')
+    parser.add_argument('-o', '--output', help='C file to which to output'
+                        ' compiled stercus.', dest='out')
     args = parser.parse_args()
 
-    tokens = preprocess(args.src)
-    if len(tokens) > 0:
-        if args.compile:
-            compile(tokens, 'out.c')
-        else:
-            run(tokens)
+    # Read the source file.
+    with open(args.src, 'r') as f:
+        src = f.read()
+
+    # Process the source code to extract functions and script parts.
+    script, functions = preprocess(src)
+
+    # C boilerplate.
+    c = '#include <stdio.h>\n#include <stdlib.h>\nunsigned char *_d;'
+
+    func_list = functions.keys()
+
+    # Compile each function.
+    for func in functions:
+        c += compile_function(func, 'unsigned int _s', functions[func], func_list)
+
+    # Compile the main function last.
+    c += compile_function('main', 'void', script, func_list)
+
+    # Write the C code out to a file.
+    if args.out:
+        out_file = args.out
+    else:
+        out_file = 'out.c'
+    with open(out_file, 'w') as f:
+        f.write(c)
+    if args.gcc:
+        call(['gcc', out_file])
+        call(['rm', out_file])
 
 if __name__ == '__main__':
     main()
